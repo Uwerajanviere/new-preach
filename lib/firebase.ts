b9d3d82984aa7c3c2be3f85a78de3793fd6cc7f0
Uwerajanviere <124252344+Uwerajanviere@users.noreply.github.com>
2025-06-13 20:21:48 +0200

    fix: update Firebase configuration and add Firebase dependencies

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,8 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAur2N4fjWLtkiM1ZFvtV2C94TNYWc-7O0",
   authDomain: "preach-13e29.firebaseapp.com",
   projectId: "preach-13e29",
-  storageBucket: "preach-13e29.firebasestorage.app",
-  messagingSenderId: "590898725445",
+  storageBucket: "preach-13e29.appspot.com",
   appId: "1:590898725445:web:eae6434cf133bb043f992c"
 };
 
